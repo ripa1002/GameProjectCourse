b8e1ea507027443deb46e50df7fe63ef9f1a2284
Risto Sutinen <risto.sutinen@gmail.com>
2018-03-24 07:16:16 +0200

    First Commit
    Added game demo to github

---
+++ Demo01/Assets/DestroyPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayer : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.name == "Player") {
+			Destroy (col.gameObject);
+			Debug.Log ("Congratulations! You managed to die!");
+		}
+	}
+}
---
+++ Demo01/Assets/MoveCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+	public float speed = 1.0f;
+	private BoxCollider _boxCollider;
+	private bool directionChanger = true;
+
+	// Use this for initialization
+	void Start () {
+		_boxCollider = GetComponent<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this._boxCollider.transform.position.x < -20.42f) {
+			this.directionChanger = false;
+		}
+		if (this._boxCollider.transform.position.x > 7.0f) {
+			this.directionChanger = true;
+		}
+		if (this._boxCollider.transform.position.x > -20.42f && directionChanger) {
+			transform.Translate (speed * -1, 0, 0, Space.World);
+		} else {
+			transform.Translate (speed, 0, 0, Space.World);
+		}
+
+	}
+}
